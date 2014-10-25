92aba732a8968abee2a1a4cab820ecfe4e941d41
Oliver <work.serv.oliver@gmail.com>
2014-10-25 23:37:28 +0800

    fine

---
+++ Puzzle22_BrowserTest/src/main/BrowserTest.java
@@ -0,0 +1,16 @@
+package main;
+
+public class BrowserTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("iexplore:");
+		//http://www.baidu.com;  //
+		System.out.print(":maximize");
+		
+		/*打印结果：iexplore::maximize
+		 * 中间的：：是url语句符号
+		 * 后面跟着尾注释*/
+	}
+
+}
